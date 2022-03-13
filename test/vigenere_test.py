988fe06181bdbdc400bcc95ed41452b7635d82af
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-03-13 09:16:24 +0530

    Add basic decrypt test for vigenere

--- test/vigenere_test.py
+++ test/vigenere_test.py
@@ -1,8 +1,11 @@
 import unittest
 import sys
 sys.path.append(".")
-from src.vigenere import encrypt
+from src.vigenere import encrypt, decrypt
 
 class VigenereTest(unittest.TestCase):
     def test_encrypt_uppercase(self):
         self.assertEqual(encrypt("WE", "CO"), "YS")
+
+    def test_decrypt_uppercase(self):
+        self.assertEqual(decrypt("YS", "CO"), "WE")
