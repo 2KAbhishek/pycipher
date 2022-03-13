7c9b503c4786a88a6b33d681fb34a7a2aae4a7d8
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-03-13 09:32:11 +0530

    Combine encrypt and decrypt to vigenere

--- src/vigenere.py
+++ src/vigenere.py
@@ -4,9 +4,9 @@ def main():
     key = input("Enter the key: ")
 
     if input("Do you want to encrypt or decrypt the message? (E/D): ") == "D":
-        print("The decrypted message is:", decrypt(message, key))
+        print("The decrypted message is:", vigenere(message, key, False))
     else:
-        print("The encrypted message is:", encrypt(message, key))
+        print("The encrypted message is:", vigenere(message, key))
 
 # Remove spaces from the message
 def remove_spaces(message):
@@ -31,27 +31,16 @@ def generate_key(message, key):
     return("" . join(key))
 
 # Encrypt the message using the key
-def encrypt(message, key):
+def vigenere(message, key, encrypt=True):
     stripped_message = remove_spaces(message)
     gen_key = generate_key(stripped_message, key)
     encrypted_message = []
     for i in range(len(stripped_message)):
-        code = (ord(stripped_message[i]) + ord(gen_key[i])) % 26
+        code = ord(stripped_message[i])
+        code = (code + ord(gen_key[i])) % 26 if encrypt else (code - ord(gen_key[i])) % 26
         code += ord('A')
         encrypted_message.append(chr(code))
     return add_spaces("" . join(encrypted_message), message)
-
-# Decrypt the message using the key
-def decrypt(message, key):
-    message = remove_spaces(message)
-    key = generate_key(message, key)
-    decypted_message = []
-    for i in range(len(message)):
-        x = (ord(message[i]) - ord(key[i]) + 26) % 26
-        x += ord('A')
-        decypted_message.append(chr(x))
-    return("" . join(decypted_message))
-
 
 if __name__ == "__main__":
     main()
