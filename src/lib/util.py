3e07efe8405796412ed05ba4ff8adf88d29e329a
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-03-13 10:20:20 +0530

    Add type hints to util

--- src/lib/util.py
+++ src/lib/util.py
@@ -1,9 +1,9 @@
 # Remove spaces from the message
-def remove_spaces(message):
+def remove_spaces(message: str) -> str:
     return("".join(message.split()))
 
 # Get back original message
-def add_spaces(processed, original):
+def add_spaces(processed: str, original: str) -> str:
     processed = list(processed)
     for i in range(len(original)):
         if original[i] == ' ':
